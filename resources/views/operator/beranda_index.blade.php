089456274a09c063040acfa398ce1667073836f9
humaidi <humaidizakaria1998@gmail.com>
2023-05-31 15:59:46 +0700

    modifikasi tampilan admin

--- resources/views/operator/beranda_index.blade.php
+++ resources/views/operator/beranda_index.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.app_sneat')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Hai Operator</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mb-4">
+            <div class="card-header">Hai Operator</div>
+
+            <div class="card-body">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+                @endif
+
+                {{ __('You are logged in!') }}
             </div>
         </div>
     </div>
+
 </div>
+
 @endsection
