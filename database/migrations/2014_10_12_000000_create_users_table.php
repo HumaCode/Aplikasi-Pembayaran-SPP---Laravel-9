4f4f349db7ef6a9336376b3c4689d0754e150126
humaidi <humaidizakaria1998@gmail.com>
2023-05-31 15:27:05 +0700

    modifikasi database & membuat view user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('akses', ['wali', 'operator', 'admin']);
+            $table->string('nohp')->unique();
+            $table->datetime('nohp_verified_at')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
