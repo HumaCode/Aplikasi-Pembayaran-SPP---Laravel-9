7f5c47e08e4e22b600cfde301b72d23bced09c41
humaidi <humaidizakaria1998@gmail.com>
2023-06-10 23:06:11 +0700

    Membuat Form Tagihan part 3 custume atribute  eloquent

--- app/Models/Biaya.php
+++ app/Models/Biaya.php
@@ -6,12 +6,26 @@ use App\Traits\HasFormatRupiah;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Biaya extends Model
 {
     use HasFactory, HasFormatRupiah;
 
     protected $guarded = [];
+    protected $append = ['nama_biaya_full'];
+
+    /**
+     * Get the user's first name.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function namaBiayaFull(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->nama . ' - ' . $this->format_rupiah('jumlah'),
+        );
+    }
 
     public function user(): BelongsTo
     {
